97159a44ec5bee34ec28a903a979428351b51e01
Jordan Christensen <jordan.c4922@gmail.com>
2019-12-19 14:17:31 -0700

    Change graphql query to fetch events

--- Community Calendar/Community Calendar/API.swift
+++ Community Calendar/Community Calendar/API.swift
@@ -3,19 +3,30 @@
 import Apollo
 import Foundation
 
-public final class FindUsersQuery: GraphQLQuery {
+public final class GetEventsQuery: GraphQLQuery {
   /// The raw GraphQL definition of this operation.
   public let operationDefinition =
     """
-    query findUsers {
+    query getEvents {
       events {
         __typename
         title
+        description
+        start
+        end
+        event_images {
+          __typename
+          url
+        }
+        urls {
+          __typename
+          url
+        }
       }
     }
     """
 
-  public let operationName = "findUsers"
+  public let operationName = "getEvents"
 
   public init() {
   }
@@ -52,6 +63,11 @@ public final class FindUsersQuery: GraphQLQuery {
       public static let selections: [GraphQLSelection] = [
         GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
         GraphQLField("title", type: .nonNull(.scalar(String.self))),
+        GraphQLField("description", type: .nonNull(.scalar(String.self))),
+        GraphQLField("start", type: .nonNull(.scalar(String.self))),
+        GraphQLField("end", type: .nonNull(.scalar(String.self))),
+        GraphQLField("event_images", type: .list(.nonNull(.object(EventImage.selections)))),
+        GraphQLField("urls", type: .list(.nonNull(.object(Url.selections)))),
       ]
 
       public private(set) var resultMap: ResultMap
@@ -60,8 +76,8 @@ public final class FindUsersQuery: GraphQLQuery {
         self.resultMap = unsafeResultMap
       }
 
-      public init(title: String) {
-        self.init(unsafeResultMap: ["__typename": "Event", "title": title])
+      public init(title: String, description: String, start: String, end: String, eventImages: [EventImage]? = nil, urls: [Url]? = nil) {
+        self.init(unsafeResultMap: ["__typename": "Event", "title": title, "description": description, "start": start, "end": end, "event_images": eventImages.flatMap { (value: [EventImage]) -> [ResultMap] in value.map { (value: EventImage) -> ResultMap in value.resultMap } }, "urls": urls.flatMap { (value: [Url]) -> [ResultMap] in value.map { (value: Url) -> ResultMap in value.resultMap } }])
       }
 
       public var __typename: String {
@@ -79,6 +95,125 @@ public final class FindUsersQuery: GraphQLQuery {
         }
         set {
           resultMap.updateValue(newValue, forKey: "title")
+        }
+      }
+
+      public var description: String {
+        get {
+          return resultMap["description"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "description")
+        }
+      }
+
+      public var start: String {
+        get {
+          return resultMap["start"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "start")
+        }
+      }
+
+      public var end: String {
+        get {
+          return resultMap["end"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "end")
+        }
+      }
+
+      public var eventImages: [EventImage]? {
+        get {
+          return (resultMap["event_images"] as? [ResultMap]).flatMap { (value: [ResultMap]) -> [EventImage] in value.map { (value: ResultMap) -> EventImage in EventImage(unsafeResultMap: value) } }
+        }
+        set {
+          resultMap.updateValue(newValue.flatMap { (value: [EventImage]) -> [ResultMap] in value.map { (value: EventImage) -> ResultMap in value.resultMap } }, forKey: "event_images")
+        }
+      }
+
+      public var urls: [Url]? {
+        get {
+          return (resultMap["urls"] as? [ResultMap]).flatMap { (value: [ResultMap]) -> [Url] in value.map { (value: ResultMap) -> Url in Url(unsafeResultMap: value) } }
+        }
+        set {
+          resultMap.updateValue(newValue.flatMap { (value: [Url]) -> [ResultMap] in value.map { (value: Url) -> ResultMap in value.resultMap } }, forKey: "urls")
+        }
+      }
+
+      public struct EventImage: GraphQLSelectionSet {
+        public static let possibleTypes = ["Event_Image"]
+
+        public static let selections: [GraphQLSelection] = [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("url", type: .nonNull(.scalar(String.self))),
+        ]
+
+        public private(set) var resultMap: ResultMap
+
+        public init(unsafeResultMap: ResultMap) {
+          self.resultMap = unsafeResultMap
+        }
+
+        public init(url: String) {
+          self.init(unsafeResultMap: ["__typename": "Event_Image", "url": url])
+        }
+
+        public var __typename: String {
+          get {
+            return resultMap["__typename"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "__typename")
+          }
+        }
+
+        public var url: String {
+          get {
+            return resultMap["url"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "url")
+          }
+        }
+      }
+
+      public struct Url: GraphQLSelectionSet {
+        public static let possibleTypes = ["Event_Url"]
+
+        public static let selections: [GraphQLSelection] = [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("url", type: .nonNull(.scalar(String.self))),
+        ]
+
+        public private(set) var resultMap: ResultMap
+
+        public init(unsafeResultMap: ResultMap) {
+          self.resultMap = unsafeResultMap
+        }
+
+        public init(url: String) {
+          self.init(unsafeResultMap: ["__typename": "Event_Url", "url": url])
+        }
+
+        public var __typename: String {
+          get {
+            return resultMap["__typename"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "__typename")
+          }
+        }
+
+        public var url: String {
+          get {
+            return resultMap["url"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "url")
+          }
         }
       }
     }
