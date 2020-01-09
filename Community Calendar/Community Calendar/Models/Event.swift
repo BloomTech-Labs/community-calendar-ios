fcfe027e1a32ebed538fc2d826bd151567ef0b17
Jordan Christensen <jordan.c4922@gmail.com>
2020-01-10 04:46:18 +0800

    Built out model in ugly way, needs to be refined

--- Community Calendar/Community Calendar/Models/Event.swift
+++ Community Calendar/Community Calendar/Models/Event.swift
@@ -9,19 +9,43 @@
 import Foundation
 
 struct Event: Codable {
+    enum EventKeys: String, CodingKey {
+        case title
+        case description
+        case images = "event_images"
+        case startDate = "start"
+        case endDate = "end"
+        case creator
+        case rsvps
+        case urls
+        case locations
+        case tags
+    }
+    
     let title: String
     let description: String
-    let images: [String]
+    let images: [Image]
     let startDate: Date
     let endDate: Date
-    let creator: String
-    let rsvps: [String]
-    let urls: [String]
+    let creator: Creator
+    let rsvps: [Rsvp]
+    let urls: [Url]
     let locations: [Location]
     let tags: [Tag]
 }
 
 struct Location: Codable {
+    enum LocationKeys: String, CodingKey {
+        case longitude
+        case latitude
+        case name
+        case state
+        case city
+        case streetAddress = "street_address"
+        case streetAddress2 = "street_address_2"
+        case zipcode
+    }
+    
     let longitude: Double
     let latitude: Double
     let name: String
@@ -30,7 +54,34 @@ struct Location: Codable {
     let streetAddress: String
     let streetAddress2: String
     let zipcode: Int
-    let tags: [String]
+}
+
+struct Creator: Codable {
+    enum CreatorKeys: String, CodingKey {
+        case firstName = "first_name"
+        case lastName = "last_name"
+    }
+    
+    let firstName: String
+    let lastName: String
+}
+
+struct Image: Codable {
+    let url: String
+}
+
+struct Rsvp: Codable {
+    enum RsvpKeys: String, CodingKey {
+        case firstName = "first_name"
+        case lastName = "last_name"
+    }
+    
+    let firstName: String
+    let lastName: String
+}
+
+struct Url: Codable {
+    let url: String
 }
 
 struct Tag: Codable {
