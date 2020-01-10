6957256524e9301f5e3dd18dae1e4fce8749ae1f
Jordan Christensen <jordan.c4922@gmail.com>
2020-01-11 03:36:12 +0800

    Add open in Calendar functionality and get permission to use calendar

--- Community Calendar/Community Calendar/View Controllers/EventDetailViewController.swift
+++ Community Calendar/Community Calendar/View Controllers/EventDetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import EventKit
 
 class EventDetailViewController: UIViewController {
     
@@ -51,6 +52,48 @@ class EventDetailViewController: UIViewController {
     }
     
     @IBAction func showInCalendar(_ sender: Any) {
+        let eventStore = EKEventStore()
+        eventStore.requestAccess(to: .event) { (granted, error) in
+            if let error = error {
+                NSLog("\(#file):L\(#line): Unable to request access to calendar in \(#function) with error: \(error)")
+                return
+            }
+
+            if granted {
+                guard let event = self.event else { return }
+                
+                let calendarEvent = EKEvent(eventStore: eventStore)
+
+                calendarEvent.title = event.title
+                calendarEvent.startDate = event.startDate
+                calendarEvent.endDate = event.endDate
+                calendarEvent.notes = event.description
+                calendarEvent.calendar = eventStore.defaultCalendarForNewEvents
+                do {
+                    try eventStore.save(calendarEvent, span: .thisEvent)
+                } catch let error {
+                    print("Failed to save event with error: \(error)")
+                    return
+                }
+                DispatchQueue.main.async {
+                    self.openCalendar(with: calendarEvent.startDate)
+                }
+            }
+            else {
+                print("Access to calendar is not granted on device!") // TODO: Alert user and link to settings to change permissions
+            }
+        }
+    }
+    
+    func openCalendar(with date: Date) {
+        let alert = UIAlertController(title: "Open Calendar?", message: "The event has been added successfully. Would you like to view it in the Calendar?", preferredStyle: .alert)
         
+        alert.addAction(UIAlertAction(title: "No", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { _ in
+            let interval = date.timeIntervalSinceReferenceDate
+            let url = URL(string: "calshow:\(interval)")!
+            UIApplication.shared.open(url)
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
 }
